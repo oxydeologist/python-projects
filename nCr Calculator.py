75f24c593f65e1b56d6e07da9873ed27b2d41241
zul <68185772+oxydeologist@users.noreply.github.com>
2023-11-30 09:36:37 +0700

    Add files via upload

---
+++ nCr Calculator.py
@@ -0,0 +1,9 @@
+import math
+n = int(input("Please type in n: "))
+r = int(input("Please type in r: "))
+n_factorial = math.factorial(n)
+r_factorial = math.factorial(r)
+n_minus_r = n - r
+nr_factorial = math.factorial(n_minus_r)
+nCr = n_factorial / (r_factorial * nr_factorial)
+print(nCr)
